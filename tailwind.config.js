5bb29470ed1705716df7e9ec8d1d46c3e22321a4
ishantbhakre <ishantbusiness12@gmail.com>
2026-05-01 17:08:44 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#d4af37",
+        dark: "#0a0a0a",
+      },
+    },
+  },
+  plugins: [],
+};
